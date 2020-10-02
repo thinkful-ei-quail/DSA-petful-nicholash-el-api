95542cb978ca04ee89bd79cff74d0b2f77fca2ab
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-10-02 13:56:57 -0500

    cors

--- modules/app/app.js
+++ modules/app/app.js
@@ -8,9 +8,7 @@ const app = express();
 
 app.use(morgan('tiny'));
 
-app.use(cors({
-  origin: config.CLIENT_ORIGIN
-}));
+app.use(cors());
 
 app.use('/api/people', require('../people/people.router'));
 app.use('/api/pets', require('../pets/pets.router'));
